e54dbde44a93e16566efeac3237be2a1e0d3b8d2
Osman Yavuz Olgun <osmanyavuzolgun@gmail.com>
2023-05-13 18:27:26 +0300

    SwipeArt :: backend operations for musician and painter preferences

--- Screens/ArtistPainter.js
+++ Screens/ArtistPainter.js
@@ -1,87 +1,153 @@
-import React, { useState } from 'react'
-import Background from '../components/Background'
+import React, { useState } from "react";
+import Background from "../components/Background";
 import {
-    Text, StyleSheet, View, TextInput, ScrollView,
-    Image, TouchableHighlight, Modal, AppRegistry, Linking, TouchableOpacity
-} from 'react-native';
-import ArtistMusicianType from './ArtistMusicianType';
-import MainPage from './MainPage';
-import ArtistDashboardPage from './ArtistDashboardPage';
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  ScrollView,
+  Image,
+  TouchableHighlight,
+  Modal,
+  AppRegistry,
+  Linking,
+  TouchableOpacity,
+} from "react-native";
+import ArtistMusicianType from "./ArtistMusicianType";
+import MainPage from "./MainPage";
+import ArtistDashboardPage from "./ArtistDashboardPage";
+import { RadioButton } from "react-native-paper";
+import { getAuth } from "firebase/auth";
+import {
+  collection,
+  doc,
+  setDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  getDoc,
+  getDocs,
+  where,
+  query,
+} from "firebase/firestore";
+import { db } from "../components/config";
 
 export default function ArtistPainter({ navigation }) {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const uid = user.uid;
+  const [digitalDesign, setdigitalDesign] = useState(false);
+  const [restoration, setrestoration] = useState(false);
+  const [graffiti, setgraffiti] = useState(false);
+  const [industrial, setindustrial] = useState(false);
 
-    return (
-        <Background>
-            <Text style={[{ color: 'white', marginBottom: 20, fontWeight: 'bold' }]} > what is your specific profession?</Text>
-            <TouchableHighlight
-                onPress={() => navigation.navigate(ArtistDashboardPage)}
-            >
-                <View style={styles.button}>
-                    <Text style={styles.textButton}>
-                        Digital Design
-                    </Text>
-                </View>
-            </TouchableHighlight>
-            <TouchableHighlight
-                onPress={() => navigation.navigate(ArtistDashboardPage)}
-            >
-                <View style={styles.button}>
-                    <Text style={styles.textButton}>
-                        Restoration
-                    </Text>
-                </View>
-            </TouchableHighlight>
+  function setPainterJob() {
+    updateDoc(doc(db, "users", uid, "artistPreference", "Painter"), {
+      digitalDesignScore: digitalDesign === true ? 1 : 0,
+      restorationScore: restoration === true ? 1 : 0,
+      graffitiScore: graffiti === true ? 1 : 0,
+      industrialScore: industrial === true ? 1 : 0,
+    })
+      .then(() => {
+        // Data saved successfully!
+        console.log("data submitted");
+        updateDoc(doc(db, "users", uid), {
+          completed: 1,
+        });
+        navigation.navigate("ArtistDashboardPage");
+      })
+      .catch((error) => {
+        // The write failed...
+        console.log("error");
+      });
+  }
 
-            <TouchableHighlight
-                onPress={() => navigation.navigate(ArtistDashboardPage)}
-            >
-                <View style={styles.button}>
-                    <Text style={styles.textButton}>
-                        Graffiti
-                    </Text>
-                </View>
-            </TouchableHighlight>
+  return (
+    <Background>
+      <Text style={[{ color: "white", marginBottom: 20, fontWeight: "bold" }]}>
+        what is your specific profession?
+      </Text>
+      <View style={styles.radioButton}>
+        <RadioButton
+          label="Digital Design"
+          value="1"
+          status={digitalDesign === true ? "checked" : "unchecked"}
+          onPress={() => setdigitalDesign(!digitalDesign)}
+        />
+        <Text style={styles.textButton}>Digital Design</Text>
+      </View>
 
-            <TouchableHighlight
-                onPress={() => navigation.navigate(ArtistDashboardPage)}
-            >
-                <View style={styles.button}>
-                    <Text style={styles.textButton}>
-                        Industrial
-                    </Text>
-                </View>
-            </TouchableHighlight>
-        </Background >
-    )
+      <View style={styles.radioButton}>
+        <RadioButton
+          label="Restoration"
+          value="1"
+          status={restoration === true ? "checked" : "unchecked"}
+          onPress={() => setrestoration(!restoration)}
+        />
+        <Text style={styles.textButton}>Restoration</Text>
+      </View>
+
+      <View style={styles.radioButton}>
+        <RadioButton
+          label="Graffiti"
+          value="1"
+          status={graffiti === true ? "checked" : "unchecked"}
+          onPress={() => setgraffiti(!graffiti)}
+        />
+        <Text style={styles.textButton}>Graffiti</Text>
+      </View>
+
+      <View style={styles.radioButton}>
+        <RadioButton
+          label="Industrial"
+          value="1"
+          status={industrial === true ? "checked" : "unchecked"}
+          onPress={() => setindustrial(!industrial)}
+        />
+        <Text style={styles.textButton}>Industrial</Text>
+      </View>
+
+      <TouchableHighlight onPress={() => setPainterJob()}>
+        <View style={styles.button}>
+          <Text style={styles.textButton}>Next</Text>
+        </View>
+      </TouchableHighlight>
+    </Background>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: 75,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-    },
-    button: {
-        width: 200,
-        marginBottom: 20,
-        marginTop: 20,
-        padding: 10,
-        backgroundColor: '#3451FF',
-        borderRadius: 15,
+  container: {
+    height: 75,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  button: {
+    width: 200,
+    marginBottom: 20,
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#3451FF",
+    borderRadius: 15,
 
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 0,
-        },
-        shadowOpacity: 0.15,
-        shadowRadius: 6.46,
-        elevation: 9,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
     },
-    textButton: {
-        color: 'white',
-    },
-})
+    shadowOpacity: 0.15,
+    shadowRadius: 6.46,
+    elevation: 9,
+  },
+  textButton: {
+    color: "white",
+  },
+  radioButton: {
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- Screens/CustomerChooseMusician.js
+++ Screens/CustomerChooseMusician.js
@@ -1,172 +1,170 @@
-import React, { useState } from 'react'
-import Background from '../components/Background'
+import React, { useState } from "react";
+import Background from "../components/Background";
 import {
-    Text, StyleSheet, View, TextInput, ScrollView,
-    Image, TouchableHighlight, Modal, AppRegistry, Linking, TouchableOpacity
-} from 'react-native';
-import { RadioButton } from 'react-native-paper';
-import MainPage from './MainPage';
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  ScrollView,
+  Image,
+  TouchableHighlight,
+  Modal,
+  AppRegistry,
+  Linking,
+  TouchableOpacity,
+} from "react-native";
+import { RadioButton } from "react-native-paper";
+import MainPage from "./MainPage";
 import { getAuth } from "firebase/auth";
-import { collection, doc, setDoc, addDoc, updateDoc, deleteDoc, getDoc, getDocs, where, query } from "firebase/firestore";
-import { db } from '../components/config';
-
+import {
+  collection,
+  doc,
+  setDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  getDoc,
+  getDocs,
+  where,
+  query,
+} from "firebase/firestore";
+import { db } from "../components/config";
 
 export default function CustomerChooseMusician({ navigation }) {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const uid = user.uid;
+  const [rock, setrock] = useState(false);
+  const [rap, setrap] = useState(false);
+  const [blues, setblues] = useState(false);
+  const [pop, setpop] = useState(false);
+  const [edm, setedm] = useState(false);
 
-    const auth = getAuth();
-    const user = auth.currentUser;
-    const uid = user.uid;
-    const [rock, setrock] = useState(false);
-    const [rap, setrap] = useState(false);
-    const [blues, setblues] = useState(false);
-    const [pop, setpop] = useState(false);
-    const [edm, setedm] = useState(false);
+  // THIS SECTION ONLY SETS INITIAL SCORES
+  // REST WILL CHANGE ON ALGORITHM
+  function setMusicianPreference() {
+    updateDoc(doc(db, "users", uid, "userPreference", "MusicianTypes"), {
+      rapScore: rap === true ? 1 : 0,
+      rockScore: rock === true ? 1 : 0,
+      popScore: pop === true ? 1 : 0,
+      edmScore: edm === true ? 1 : 0,
+      bluesScore: blues === true ? 1 : 0,
+    })
+      .then(() => {
+        // Data saved successfully!
+        console.log("data submitted");
+        updateDoc(doc(db, "users", uid), {
+          completed: 1,
+        });
+        navigation.navigate(MainPage);
+      })
+      .catch((error) => {
+        // The write failed...
+        console.log("error");
+      });
+  }
 
- // THIS SECTION ONLY SETS INITIAL SCORES
- // REST WILL CHANGE ON ALGORITHM
-    function setMusicianPreference() {
-            updateDoc(doc(db, "users", uid , "userPreference", "MusicianTypes"), {  
-                rapScore: rap === true ? 1 : 0,
-                rockScore: rock === true ? 1 : 0,
-                popScore: pop === true ? 1 : 0,
-                edmScore: edm === true ? 1 : 0,
-                bluesScore: blues === true ? 1 : 0,
-            }).then(() => { 
-            // Data saved successfully!
-            console.log('data submitted');
-            updateDoc(doc(db, "users", uid), {  
-                completed: 1,
-            })    
-            navigation.navigate(MainPage)        
-            }).catch((error) => { 
-                // The write failed...
-            console.log("error");
-            });
-    }
+  return (
+    <Background>
+      <Text style={[{ color: "white", marginBottom: 20, fontWeight: "bold" }]}>
+        which specific profession are you looking for?
+      </Text>
+      <View style={styles.container}>
+        <RadioButton
+          label="Pop"
+          value="1"
+          status={pop === true ? "checked" : "unchecked"}
+          onPress={() => setpop(!pop)}
+        />
+        <Text style={styles.textButton}>POP</Text>
+      </View>
+      <View style={styles.container}>
+        <RadioButton
+          label="Rock"
+          value="1"
+          status={rock === true ? "checked" : "unchecked"}
+          onPress={() => setrock(!rock)}
+        />
+        <Text style={styles.textButton}>Rock</Text>
+      </View>
+      <View style={styles.container}>
+        <RadioButton
+          label="Rap"
+          value="1"
+          status={rap === true ? "checked" : "unchecked"}
+          onPress={() => setrap(!rap)}
+        />
+        <Text style={styles.textButton}>Rap</Text>
+      </View>
+      <View style={styles.container}>
+        <RadioButton
+          label="blues"
+          value="1"
+          status={blues === true ? "checked" : "unchecked"}
+          onPress={() => setblues(!blues)}
+        />
+        <Text style={styles.textButton}>Blues</Text>
+      </View>
+      <View style={styles.container}>
+        <RadioButton
+          label="edm"
+          value="1"
+          status={edm === true ? "checked" : "unchecked"}
+          onPress={() => setedm(!edm)}
+        />
+        <Text style={styles.textButton}>EDM-Techno</Text>
+      </View>
 
+      <TouchableHighlight onPress={() => setMusicianPreference()}>
+        <View style={styles.button}>
+          <Text style={styles.textButton}>Next</Text>
+        </View>
+      </TouchableHighlight>
 
-    return (
-        <Background>
-            <Text style={[{ color: 'white', marginBottom: 20, fontWeight: 'bold' }]} > which specific profession are you looking for?</Text>
-            <View style={styles.container}>
-                <RadioButton 
-                    label="Pop"
-                    value="1"
-                    status={ pop === true ? 'checked' : 'unchecked' }
-                    onPress={() => setpop(!pop)}
-                    />
-                    <Text style={styles.textButton}>
-                        POP
-                    </Text>
-            </View>
-            <View style={styles.container}>
-                <RadioButton 
-                    label="Rock"
-                    value="1"
-                    status={ rock === true ? 'checked' : 'unchecked' }
-                    onPress={() => setrock(!rock)}
-                    />
-                    <Text style={styles.textButton}>
-                        Rock
-                    </Text>
-            </View>
-            <View style={styles.container}>
-                <RadioButton 
-                    label="Rap"
-                    value="1"
-                    status={ rap === true ? 'checked' : 'unchecked' }
-                    onPress={() => setrap(!rap)}
-                    />
-                    <Text style={styles.textButton}>
-                        Rap
-                    </Text>
-            </View>
-            <View style={styles.container}>
-                <RadioButton 
-                    label="blues"
-                    value="1"
-                    status={ blues === true ? 'checked' : 'unchecked' }
-                    onPress={() => setblues(!blues)}
-                    />
-                    <Text style={styles.textButton}>
-                        Blues
-                    </Text>
-            </View>
-            <View style={styles.container}>
-                <RadioButton 
-                    label="edm"
-                    value="1"
-                    status={ edm === true ? 'checked' : 'unchecked' }
-                    onPress={() => setedm(!edm)}
-                    />
-                    <Text style={styles.textButton}>
-                        EDM-Techno
-                    </Text>
-            </View>
-
-            <TouchableHighlight
-                onPress={() => setMusicianPreference()}
-            >
-                <View style={styles.button}>
-                    <Text style={styles.textButton}>
-                        Next
-                    </Text>
-                </View>
-            </TouchableHighlight>
-
-
-            <TouchableHighlight
-                onPress={() => navigation.navigate(MainPage)}
-            >
-                <View >
-                    <Text style={styles.textButton2}>
-                        Others+
-                    </Text>
-                    <Text style={styles.textButton2}>
-                        feel free to contact us!
-                    </Text>
-                </View>
-            </TouchableHighlight>
-
-        </Background >
-    )
+      <TouchableHighlight onPress={() => navigation.navigate(MainPage)}>
+        <View>
+          <Text style={styles.textButton2}>Others+</Text>
+          <Text style={styles.textButton2}>feel free to contact us!</Text>
+        </View>
+      </TouchableHighlight>
+    </Background>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: 75,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-    },
-    button: {
-        width: 200,
-        marginBottom: 20,
-        marginTop: 20,
-        padding: 10,
-        backgroundColor: '#3451FF',
-        borderRadius: 15,
+  container: {
+    height: 75,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  button: {
+    width: 200,
+    marginBottom: 20,
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#3451FF",
+    borderRadius: 15,
 
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 0,
-        },
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
 
-        shadowOpacity: 0.15,
-        shadowRadius: 6.46,
-        elevation: 9,
-
-    },
-    textButton: {
-        color: 'white',
-    },
-    textButton2: {
-        color: 'white',
-        textAlign: 'center',
-        marginTop: 20,
-        fontWeight: 'bold',
-    },
-})
+    shadowOpacity: 0.15,
+    shadowRadius: 6.46,
+    elevation: 9,
+  },
+  textButton: {
+    color: "white",
+  },
+  textButton2: {
+    color: "white",
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "bold",
+  },
+});
